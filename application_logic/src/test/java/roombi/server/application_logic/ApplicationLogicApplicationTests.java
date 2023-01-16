929ed8810ef205a596edb990dba7489b3fed5cad
Jin Kwon Kim <cosy363@naver.com>
2023-01-16 12:30:12 +0900

    Initialize
    - Initialize Gradle Setting

---
+++ application_logic/src/test/java/roombi/server/application_logic/ApplicationLogicApplicationTests.java
@@ -0,0 +1,13 @@
+package roombi.server.application_logic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationLogicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
